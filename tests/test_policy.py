7a7bb77aef6e053bb854ae3a07220b3aba60cada
Thiago P. Bueno <thiago.pbueno@gmail.com>
2018-08-09 14:35:01 -0400

    Change OpenLoopPolicy to build all policy variables before being __call__'ed

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -14,13 +14,11 @@
 # along with tf-plan. If not, see <http://www.gnu.org/licenses/>.
 
 
-import tensorflow as tf
-
 from pyrddl.parser import RDDLParser
 from tfrddlsim.compiler import Compiler
-
 from tfplan.train.policy import OpenLoopPolicy
 
+import tensorflow as tf
 import unittest
 
 
@@ -43,17 +41,16 @@ class TestOpenLoopPolicy(unittest.TestCase):
         cls.rddl2tf = Compiler(rddl, batch_mode=True)
 
         # initialize open-loop policy
-        cls.policy = OpenLoopPolicy(cls.rddl2tf, cls.batch_size)
+        cls.policy = OpenLoopPolicy(cls.rddl2tf, cls.batch_size, cls.horizon)
 
         # execute policy for the given horizon and initial state
         with cls.rddl2tf.graph.as_default():
             cls.state = cls.rddl2tf.compile_initial_state(cls.batch_size)
             cls.actions = []
-            for t in range(cls.horizon, 0, -1):
+            for t in range(cls.horizon-1, -1, -1):
                 timestep = tf.constant(t, dtype=tf.float32, shape=(cls.batch_size, 1))
-                with tf.variable_scope('timestep{}'.format(t)):
-                    action = cls.policy(cls.state, timestep)
-                    cls.actions.append(action)
+                action = cls.policy(cls.state, timestep)
+                cls.actions.append(action)
 
     def test_policy_variables(self):
         action_fluents = self.rddl2tf.action_fluent_ordering
@@ -63,31 +60,32 @@ class TestOpenLoopPolicy(unittest.TestCase):
             policy_variables = tf.trainable_variables()
             name2variable = { var.name: var for var in policy_variables }
 
-            self.assertEqual(len(policy_variables), self.horizon * len(action_fluents),
-                'one variable per action fluent per timestep')
+            self.assertEqual(len(policy_variables), len(action_fluents),
+                'one variable per action fluent')
 
-            for i, t in enumerate(range(self.horizon, 0, -1)):
-                scope = 'timestep{}/'.format(t)
-                policy_variables = tf.trainable_variables(scope=scope)
-                action = self.actions[i]
+            for fluent, size, var in zip(action_fluents, action_size, policy_variables):
+                var_name = fluent.replace('/', '-') + ':0'
+                self.assertIn(var_name, name2variable, 'variable has fluent name')
 
-                self.assertEqual(len(policy_variables), len(action_fluents),
-                    'one variable per action fluent in each timestep')
+                self.assertIsInstance(var, tf.Variable,
+                    'policy variable is a variable tensor')
 
-                self.assertEqual(len(action), len(action_fluents),
-                    'one action tensor per action fluent in each timestep')
+                shape = [self.batch_size, self.horizon] + list(size)
+                self.assertListEqual(var.shape.as_list(), shape,
+                    'policy variable has shape (batch_size, horizon, action_fluent_shape')
 
-                for fluent, shape, tensor in zip(action_fluents, action_size, action):
-                    self.assertIsInstance(tensor, tf.Tensor, 'action fluent is a tf.Tensor')
+    def test_policy_actions(self):
+        action_fluents = self.rddl2tf.action_fluent_ordering
+        action_size = self.rddl2tf.action_size
 
-                    var_name = scope + fluent.replace('/', '-') + ':0'
-                    self.assertIn(var_name, name2variable,
-                        'variable name is the concatenation of timestep scope and fluent name')
+        for action in self.actions:
+            self.assertIsInstance(action, tuple, 'action is factored')
+            self.assertEqual(len(action), len(action_fluents),
+                'one action tensor per action fluent in each timestep')
 
-                    var = name2variable[var_name]
-                    shape = [self.batch_size] + list(shape)
-                    self.assertListEqual(var.shape.as_list(), shape,
-                        'policy variable has shape (batch_size, fluent_shape)')
-                    self.assertListEqual(tensor.shape.as_list(), shape,
-                        'action tensor has shape (batch_size, fluent_shape)')
+            for fluent, size, tensor in zip(action_fluents, action_size, action):
+                self.assertIsInstance(tensor, tf.Tensor, 'action fluent is a tf.Tensor')
 
+                shape = [self.batch_size] + list(size)
+                self.assertListEqual(tensor.shape.as_list(), shape,
+                    'action tensor has shape (batch_size, fluent_shape)')
--- tfplan/train/policy.py
+++ tfplan/train/policy.py
@@ -26,57 +26,69 @@ Bounds = Tuple[Optional[TensorFluent], Optional[TensorFluent]]
 
 
 class OpenLoopPolicy(Policy):
-    '''OpenLoopPolicy implements a policy that returns an action
-    regarless of what the current state is.
+    '''OpenLoopPolicy returns an action independently of the current state.
 
     Note:
-        It uses the current state for constraining the bounds of each action fluent.
+        It uses the current state only for constraining the bounds of each action fluent.
 
     Args:
         compiler (:obj:`tfrddlsim.compiler.Compiler`): A RDDL2TensorFlow compiler.
         batch_size (int): The batch size.
+        horizon(int): The number of timesteps.
     '''
 
-    def __init__(self, compiler: Compiler, batch_size: int) -> None:
+    def __init__(self, compiler: Compiler, batch_size: int, horizon: int) -> None:
         self._compiler = compiler
         self._batch_size = batch_size
+        self._horizon = horizon
+
+        self._build_policy_variables()
+
+    def _build_policy_variables(self) -> None:
+        '''Builds the policy variables for each action fluent.'''
+        action_fluents = self._compiler.action_fluent_ordering
+        action_size = self._compiler.action_size
+        with self._compiler.graph.as_default():
+            self._policy_variables = []
+            for fluent, shape in zip(action_fluents, action_size):
+                var = self._get_policy_variable(fluent, shape)
+                self._policy_variables.append(var)
+            self._policy_variables = tuple(self._policy_variables)
 
     def __call__(self,
             state: Sequence[tf.Tensor],
             timestep: tf.Tensor) -> Sequence[tf.Tensor]:
         '''Returns action fluents for the current `state` and `timestep`.
 
+        Args:
+            state (Sequence[tf.Tensor]): The current state fluents.
+            timestep (tf.Tensor): The current timestep.
+
         Returns:
             Sequence[tf.Tensor]: A tuple of action fluents.
         '''
+        action_fluents = self._compiler.action_fluent_ordering
         bounds = self._compiler.compile_action_bound_constraints(state)
         action = []
         with self._compiler.graph.as_default():
-            action_fluents = self._compiler.action_fluent_ordering
-            action_size = self._compiler.action_size
-            for fluent, shape in zip(action_fluents, action_size):
-                var = self._get_policy_variable(fluent, shape, self._batch_size)
-                tensor = self._get_action_tensor(var, bounds[fluent])
+            t = tf.cast(timestep[0][0], tf.int32) # TODO change timestep dtype in tfrddlsim
+            for fluent, var in zip(action_fluents, self._policy_variables):
+                tensor = self._get_action_tensor(var[:,t,:], bounds[fluent])
                 action.append(tensor)
-        return action
+        return tuple(action)
 
-    def _get_policy_variable(self,
-            fluent: str,
-            fluent_shape: Sequence[int],
-            batch_size: int) -> tf.Tensor:
-        '''Returns the correspondig policy variable for `fluent` with `fluent_shape`
-        for the given `batch_size`.
+    def _get_policy_variable(self, fluent: str, fluent_shape: Sequence[int]) -> tf.Tensor:
+        '''Returns the correspondig policy variable for `fluent` with `fluent_shape`.
 
         Args:
             fluent (str): The fluent name.
             fluent_shape (Sequence[int]): The fluent shape.
-            batch_size (int): The size of the batch.
 
         Returns:
             tf.Tensor: The policy variable for the action fluent.
         '''
-        shape = [batch_size] + list(fluent_shape)
-        name = fluent.replace('/', '-')
+        shape = [self._batch_size, self._horizon] + list(fluent_shape)
+        name = fluent.replace('/', '-') # TODO change canonical fluent name in tfrddlsim
         return tf.get_variable(name, dtype=tf.float32, shape=shape)
 
     def _get_action_tensor(self, policy_variable: tf.Tensor, bounds: Bounds) -> tf.Tensor:
