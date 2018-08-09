06c3462fe011ec015eb610a481a3b8380c79ade4
Thiago P. Bueno <thiago.pbueno@gmail.com>
2018-08-09 17:02:41 -0400

    Add ActionOptimizer based on RMSProp

---
+++ tests/test_optimizer.py
@@ -0,0 +1,130 @@
+# This file is part of tf-plan.
+
+# tf-plan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-plan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-plan. If not, see <http://www.gnu.org/licenses/>.
+
+
+from pyrddl.parser import RDDLParser
+from tfrddlsim.compiler import Compiler
+from tfplan.train.policy import OpenLoopPolicy
+from tfplan.train.optimizer import ActionOptimizer
+
+import numpy as np
+import tensorflow as tf
+import unittest
+
+
+class TestActionOptimizer(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        # initialize hyper-parameters
+        cls.horizon = 40
+        cls.batch_size = 64
+        cls.epochs = 50
+
+        # parse RDDL file
+        with open('rddl/Navigation.rddl') as file:
+            parser = RDDLParser()
+            parser.build()
+            rddl = parser.parse(file.read())
+
+        # initializer RDDL2TensorFlow compiler
+        cls.rddl2tf = Compiler(rddl, batch_mode=True)
+
+        # initialize open-loop policy
+        cls.policy = OpenLoopPolicy(cls.rddl2tf, cls.batch_size, cls.horizon)
+
+        # initialize ActionOptimizer
+        cls.optimizer = ActionOptimizer(cls.rddl2tf, cls.policy)
+        cls.optimizer.build(cls.horizon)
+
+    def test_state_trajectory(self):
+        states = self.optimizer.states
+        state_size = self.rddl2tf.state_size
+        self.assertIsInstance(states, tuple, 'state trajectory is factored')
+        self.assertEqual(len(states), len(state_size), 'state trajectory has all states fluents')
+        for fluent, fluent_size in zip(states, state_size):
+            tensor_size = [self.batch_size, self.horizon] + list(fluent_size)
+            self.assertIsInstance(fluent, tf.Tensor, 'state fluent is a tensor')
+            self.assertListEqual(fluent.shape.as_list(), tensor_size, 'fluent size is [batch_size, horizon, state_fluent_size]')
+
+    def test_interm_trajectory(self):
+        interms = self.optimizer.interms
+        interm_size = self.rddl2tf.interm_size
+        self.assertIsInstance(interms, tuple, 'interm trajectory is factored')
+        self.assertEqual(len(interms), len(interm_size),
+            'interm trajectory has all interms fluents')
+        for fluent, fluent_size in zip(interms, interm_size):
+            tensor_size = [self.batch_size, self.horizon] + list(fluent_size)
+            self.assertIsInstance(fluent, tf.Tensor, 'state fluent is a tensor')
+            self.assertListEqual(fluent.shape.as_list(), tensor_size,
+                'fluent size is [batch_size, horizon, interm_fluent_size]')
+
+    def test_action_trajectory(self):
+        actions = self.optimizer.actions
+        action_size = self.rddl2tf.action_size
+        self.assertIsInstance(actions, tuple, 'action trajectory is factored')
+        self.assertEqual(len(actions), len(action_size),
+            'action trajectory has all actions fluents')
+        for fluent, fluent_size in zip(actions, action_size):
+            tensor_size = [self.batch_size, self.horizon] + list(fluent_size)
+            self.assertIsInstance(fluent, tf.Tensor, 'action fluent is a tensor')
+            self.assertListEqual(fluent.shape.as_list(), tensor_size,
+                'fluent size is [batch_size, horizon, action_fluent_size]')
+
+    def test_rewards_trajectory(self):
+        rewards = self.optimizer.rewards
+        rewards_shape = [self.batch_size, self.horizon, 1]
+        self.assertIsInstance(rewards, tf.Tensor, 'reward trajectory is a tensor')
+        self.assertListEqual(rewards.shape.as_list(), rewards_shape ,
+            'reward shape is [batch_size, horizon, 1]')
+
+    def test_optimization_variables(self):
+        action_size = self.rddl2tf.action_size
+        with self.rddl2tf.graph.as_default():
+            policy_variables = tf.trainable_variables()
+            self.assertEqual(len(policy_variables), len(action_size),
+                'one variable per action fluent per timestep')
+
+    def test_total_reward(self):
+        total_reward = self.optimizer.total_reward
+        self.assertIsInstance(total_reward, tf.Tensor, 'total reward is a tensor')
+        self.assertEqual(total_reward.dtype, tf.float32, 'total reward is a real tensor')
+        self.assertListEqual(total_reward.shape.as_list(), [self.batch_size, 1],
+            'total reward has shape [batch_size, 1]')
+
+    def test_avg_total_reward(self):
+        avg_total_reward = self.optimizer.avg_total_reward
+        self.assertIsInstance(avg_total_reward, tf.Tensor, 'average total reward is a tensor')
+        self.assertEqual(avg_total_reward.dtype, tf.float32, 'average total reward is a real tensor')
+        self.assertListEqual(avg_total_reward.shape.as_list(), [], 'average total reward is a scalar')
+
+    def test_optimization_objective(self):
+        loss = self.optimizer.loss
+        self.assertIsInstance(loss, tf.Tensor, 'loss function is a tensor')
+        self.assertEqual(loss.dtype, tf.float32, 'loss function is a real tensor')
+        self.assertListEqual(loss.shape.as_list(), [], 'loss function is a scalar')
+
+    def test_loss_optimizer(self):
+        optimizer = self.optimizer._optimizer
+        self.assertIsInstance(optimizer, tf.train.RMSPropOptimizer)
+        train_op = self.optimizer._train_op
+        self.assertEqual(train_op.name, 'RMSProp')
+
+    def test_optimizer_solution(self):
+        losses = self.optimizer.run(self.epochs, show_progress=False)
+        self.assertIsInstance(losses, list)
+        self.assertEqual(len(losses), self.epochs)
+        self.assertTrue(all(isinstance(l, np.float32) for l in losses))
---
+++ tfplan/train/optimizer.py
@@ -0,0 +1,109 @@
+# This file is part of tf-plan.
+
+# tf-plan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-plan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-plan. If not, see <http://www.gnu.org/licenses/>.
+
+
+from tfplan.train.policy import OpenLoopPolicy
+
+from tfrddlsim.compiler import Compiler
+from tfrddlsim.simulator import Simulator
+
+import numpy as np
+import tensorflow as tf
+
+from typing import List
+
+
+class ActionOptimizer(object):
+    '''ActionOptimizer implements a wrapper around RMSProp for optimizing actions.
+
+    It uses tfrddlsim package to generate trajectories for the RDDL MDP, and
+    optimizes the variables of an open loop policy in order to maximize the
+    total (undiscounted) reward received from start state.
+
+    Note:
+        For details please refer to NIPS 2017 paper:
+        "Scalable Planning with Tensorflow for Hybrid Nonlinear Domains".
+
+    Args:
+        compiler (:obj:`tfrddlsim.compiler.Compiler`): A RDDL2TensorFlow compiler.
+        policy (:obj:`tfplan.train.policy.OpenLoopPolicy`): A sequence of actions
+        implemented as an open loop policy.
+    '''
+
+    def __init__(self, compiler: Compiler, policy: OpenLoopPolicy) -> None:
+        self._compiler = compiler
+        self._policy = policy
+
+    @property
+    def graph(self) -> tf.Graph:
+        '''Returns the compiler's graph.'''
+        return self._compiler.graph
+
+    @property
+    def batch_size(self):
+        '''Returns the policy's batch size.'''
+        return self._policy._batch_size
+
+    def build(self, horizon: int, learning_rate: float = 0.001) -> None:
+        '''Builds all graph operations necessary for optimizing actions.
+
+        Args:
+            horizon (int): The number of timesteps.
+            learning_rate (int): The learning rate passed to the underlying optimizer.
+        '''
+        with self.graph.as_default():
+            self._build_trajectory_graph(horizon)
+            self._build_loss_graph()
+            self._build_optimization_graph(learning_rate)
+
+    def run(self, epochs: int, show_progress: bool = True) -> List[np.float32]:
+        '''Runs the optimization ops for a given number of training `epochs`.
+
+        Args:
+            epochs (int): The number of training epochs.
+            show_progress (bool): The boolean flag for showing intermediate results.
+
+        Returns:
+            List[np.float32]: The loss function over time.
+        '''
+        with tf.Session(graph=self.graph) as sess:
+            sess.run(tf.global_variables_initializer())
+            losses = []
+            for step in range(epochs):
+                _, loss = sess.run([self._train_op, self.loss])
+                losses.append(loss)
+                if show_progress:
+                    print('Epoch {0:5}: loss = {1}\r'.format(step, loss), end='')
+            return losses
+
+    def _build_trajectory_graph(self, horizon: int) -> None:
+        '''Builds the (state, action, interm, reward) trajectory ops.'''
+        simulator = Simulator(self._compiler, self._policy, self.batch_size)
+        trajectories = simulator.trajectory(horizon)
+        self.states = trajectories[1]
+        self.actions = trajectories[2]
+        self.interms = trajectories[3]
+        self.rewards = trajectories[4]
+
+    def _build_loss_graph(self) -> None:
+        '''Builds the loss ops.'''
+        self.total_reward = tf.reduce_sum(self.rewards, axis=1)
+        self.avg_total_reward = tf.reduce_mean(self.total_reward)
+        self.loss = -self.avg_total_reward
+
+    def _build_optimization_graph(self, learning_rate: float) -> None:
+        '''Builds the training ops.'''
+        self._optimizer = tf.train.RMSPropOptimizer(learning_rate)
+        self._train_op = self._optimizer.minimize(self.loss)
