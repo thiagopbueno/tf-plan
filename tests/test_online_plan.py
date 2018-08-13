4de48245f8c3d7e9a567ae58884e99873ac687e8
Thiago P. Bueno <thiago.pbueno@gmail.com>
2018-08-13 15:41:53 -0400

    Add OnlinePlanning class for plan-execute-monitor cycle

---
+++ tests/test_online_plan.py
@@ -0,0 +1,121 @@
+# This file is part of tf-plan.
+
+# tf-plan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-plan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-plan. If not, see <http://www.gnu.org/licenses/>.
+
+
+from pyrddl.parser import RDDLParser
+from tfrddlsim.compiler import Compiler
+
+from tfplan.planners.online_plan import OnlinePlanning
+
+import numpy as np
+import tensorflow as tf
+
+import unittest
+
+
+class TestOnlinePlanning(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        parser = RDDLParser()
+        parser.build()
+
+        with open('rddl/Navigation.rddl', mode='r') as file:
+            NAVIGATION = file.read()
+            cls.rddl = parser.parse(NAVIGATION)
+
+        cls.compiler = Compiler(cls.rddl, batch_mode=True)
+        cls.initial_state = cls.compiler.compile_initial_state(batch_size=1)
+        cls.default_action = cls.compiler.compile_default_action(batch_size=1)
+
+    def setUp(self):
+        with tf.Session(graph=self.compiler.graph) as sess:
+            default_action = sess.run(self.default_action)
+        planner = lambda _: default_action
+        self.online_planner = OnlinePlanning(self.compiler, planner)
+        self.online_planner.build()
+
+    @unittest.skip('not implemented')
+    def test_planning_graph(self):
+        self.fail()
+
+    def test_execution_graph(self):
+        action = self.online_planner.action
+        self.assertIsInstance(action, tuple)
+        self.assertEqual(len(action), len(self.default_action))
+        self.assertTrue(all(isinstance(fluent, tf.Tensor) for fluent in action))
+        for action_fluent, default_action_fluent in zip(action, self.default_action):
+            self.assertEqual(action_fluent.shape, default_action_fluent.shape)
+            self.assertEqual(action_fluent.dtype, default_action_fluent.dtype)
+
+        state = self.online_planner.state
+        self.assertIsInstance(state, tuple)
+        self.assertEqual(len(state), len(self.initial_state))
+        self.assertTrue(all(isinstance(fluent, tf.Tensor) for fluent in state))
+        for state_fluent, initial_state_fluent in zip(state, self.initial_state):
+            self.assertEqual(state_fluent.shape, initial_state_fluent.shape)
+            self.assertEqual(state_fluent.dtype, initial_state_fluent.dtype)
+
+        next_state = self.online_planner.next_state
+        self.assertIsInstance(next_state, tuple)
+        self.assertEqual(len(next_state), len(self.initial_state))
+        self.assertTrue(all(isinstance(fluent, tf.Tensor) for fluent in next_state))
+        for state_fluent, initial_state_fluent in zip(next_state, self.initial_state):
+            self.assertEqual(state_fluent.shape, initial_state_fluent.shape)
+            self.assertEqual(state_fluent.dtype, initial_state_fluent.dtype)
+
+        reward = self.online_planner.reward
+        self.assertIsInstance(reward, tuple)
+        self.assertEqual(len(reward), 1)
+        self.assertIsInstance(reward[0], tf.Tensor)
+        self.assertListEqual(reward[0].shape.as_list(), [1, 1])
+        self.assertEqual(reward[0].dtype, tf.float32)
+
+    @unittest.skip('not implemented')
+    def test_monitoring_graph(self):
+        self.fail()
+
+    def test_online_planning_cycle(self):
+        horizon = 40
+        initial_state, states, actions, rewards = self.online_planner.run(self.initial_state, horizon)
+        self.assertEqual(len(states), horizon)
+        self.assertEqual(len(actions), horizon)
+        self.assertEqual(len(rewards), horizon)
+
+        for state in states:
+            self.assertIsInstance(state, tuple)
+            self.assertEqual(len(state), len(self.initial_state))
+            self.assertTrue(all(isinstance(fluent, np.ndarray) for fluent in state))
+            for state_fluent, initial_state_fluent in zip(state, self.initial_state):
+                self.assertEqual(state_fluent.shape, initial_state_fluent.shape)
+                if initial_state_fluent.dtype == tf.float32:
+                    self.assertEqual(state_fluent.dtype, np.float32)
+                elif initial_state_fluent.dtype == tf.int32:
+                    self.assertEqual(state_fluent.dtype, np.int32)
+                elif initial_state_fluent.dtype == tf.bool:
+                    self.assertEqual(state_fluent.dtype, np.bool)
+
+        for action in actions:
+            self.assertIsInstance(action, tuple)
+            self.assertEqual(len(action), len(self.default_action))
+            self.assertTrue(all(isinstance(fluent, np.ndarray) for fluent in action))
+            for action_fluent, default_action in zip(action, self.default_action):
+                self.assertEqual(action_fluent.shape, default_action.shape)
+                if default_action.dtype == tf.float32:
+                    self.assertEqual(action_fluent.dtype, np.float32)
+                elif default_action.dtype == tf.int32:
+                    self.assertEqual(action_fluent.dtype, np.int32)
+                elif default_action.dtype == tf.bool:
+                    self.assertEqual(action_fluent.dtype, np.bool)
---
+++ tfplan/planners/online_plan.py
@@ -0,0 +1,91 @@
+# This file is part of tf-plan.
+
+# tf-plan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# tf-plan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with tf-plan. If not, see <http://www.gnu.org/licenses/>.
+
+
+from tfrddlsim.simulation.transition_simulator import ActionSimulationCell
+
+import tensorflow as tf
+
+
+class OnlinePlanning(object):
+    '''OnlinePlanning implements the plan-execute-monitor cycle.
+
+    Args:
+        compiler:
+        planner:
+    '''
+
+    def __init__(self, compiler, planner):
+        self._compiler = compiler
+        self._planner = planner
+
+    @property
+    def graph(self):
+        return self._compiler.graph
+
+    def build(self):
+        with self.graph.as_default():
+            self._build_execution_graph()
+
+    def run(self, initial_state, horizon):
+        states = []
+        actions = []
+        rewards = []
+
+        with tf.Session(graph=self.graph) as sess:
+            initial_state = sess.run(initial_state)
+
+            state = initial_state
+            for step in range(horizon):
+                # plan
+                action = self._planner(state)
+                actions.append(action)
+
+                # execute
+                feed_dict = { self.state: state, self.action: action }
+                next_state, reward = sess.run([self.next_state, self.reward], feed_dict=feed_dict)
+                states.append(next_state)
+                rewards.append(reward[0])
+
+                # monitor
+                state = next_state
+
+            return initial_state, states, actions, rewards
+
+    def _build_execution_graph(self):
+        self._transition = ActionSimulationCell(self._compiler)
+        self.state = self._build_state_inputs()
+        self.action = self._build_action_inputs()
+        self.reward, self.next_state = self._transition(self.action, self.state)
+
+    def _build_state_inputs(self):
+        fluents = self._compiler.state_fluent_ordering
+        sizes = self._compiler.state_size
+        dtypes = self._compiler.state_dtype
+        state_inputs = []
+        for fluent, size, dtype in zip(fluents, sizes, dtypes):
+            shape = [1, *size]
+            state_inputs.append(tf.placeholder(dtype, shape=shape))
+        return tuple(state_inputs)
+
+    def _build_action_inputs(self):
+        fluents = self._compiler.action_fluent_ordering
+        sizes = self._compiler.action_size
+        dtypes = self._compiler.action_dtype
+        action_inputs = []
+        for fluent, size, dtype in zip(fluents, sizes, dtypes):
+            shape = [1, *size]
+            action_inputs.append(tf.placeholder(dtype, shape=shape))
+        return tuple(action_inputs)
