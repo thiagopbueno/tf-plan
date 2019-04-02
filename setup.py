9f564c28394277716c8244c16e958bc1f14d7a63
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-04-02 11:30:14 -0400

    Fix specific versions of rddlgym, rddl2tf and tfrddlsim packages in requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ setup(
     scripts=['scripts/tfplan'],
     python_requires='>=3.5',
     install_requires=[
-        'rddlgym>=0.5.6',
-        'rddl2tf>=0.4.12',
-        'tfrddlsim>=0.6.11',
+        'rddlgym==0.5.6',
+        'rddl2tf==0.4.12',
+        'tfrddlsim==0.6.11',
         'tensorflow',
         'tensorflow-tensorboard',
         'typing'
