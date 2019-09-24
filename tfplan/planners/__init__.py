1c7c5e4393ff7823ce9c02cb8f13ef12b665027d
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-09-24 18:00:09 -0300

    chore: import Planner's DEFAULT_CONFIG in planners module
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- tfplan/planners/__init__.py
+++ tfplan/planners/__init__.py
@@ -1,4 +1,4 @@
 # pylint: disable=missing-docstring
 
-from tfplan.planners.planner import DEFAULT_CONFIG
+from tfplan.planners.planner import DEFAULT_CONFIG, Planner
 from tfplan.planners.stochastic.straightline import StraightLinePlanner
