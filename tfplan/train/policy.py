2d19e1f3a788682a63d8cd11b56f16271a4fa14a
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-09-24 18:15:35 -0300

    refactor(policy): remove typing annotations from policy.py
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- tfplan/train/policy.py
+++ tfplan/train/policy.py
@@ -13,21 +13,14 @@
 # You should have received a copy of the GNU General Public License
 # along with tf-plan. If not, see <http://www.gnu.org/licenses/>.
 
-import tfrddlsim
-import tfrddlsim.policy
-from rddl2tf import Compiler
-from tfrddlsim.policy import Policy
-from rddl2tf.core.fluent import TensorFluent
+# pylint: disable=missing-docstring
+
 
 import tensorflow as tf
 
-from typing import Optional, Sequence, Tuple
 
-Bounds = Tuple[Optional[TensorFluent], Optional[TensorFluent]]
-
-
-class OpenLoopPolicy(Policy):
-    '''OpenLoopPolicy returns an action independently of the current state.
+class OpenLoopPolicy:
+    """OpenLoopPolicy returns an action independently of the current state.
 
     Note:
         It uses the current state only for constraining the bounds of each action fluent.
@@ -36,31 +29,33 @@ class OpenLoopPolicy(Policy):
         compiler (:obj:`rddl2tf.compiler.Compiler`): A RDDL2TensorFlow compiler.
         batch_size (int): The simulation batch size.
         horizon(int): The number of timesteps.
-    '''
+    """
 
-    def __init__(self,
-            compiler: Compiler,
-            horizon: int,
-            parallel_plans: bool = True) -> None:
+    def __init__(self, compiler, horizon, parallel_plans=True):
         self._compiler = compiler
         self.horizon = horizon
         self.parallel_plans = parallel_plans
 
+        self._policy_variables = None
+
     @property
     def graph(self):
+        """Returns the compiler's graph."""
         return self._compiler.graph
 
     @property
     def batch_size(self):
+        """Returns the compiler's batch size."""
         return self._compiler.batch_size
 
-    def build(self, scope: str, initializers=None) -> None:
+    def build(self, scope, initializers=None):
+        """Builds the policy."""
         with self.graph.as_default():
             with tf.variable_scope(scope):
                 self._build_policy_variables(initializers)
 
-    def _build_policy_variables(self, initializers=None) -> None:
-        '''Builds the policy variables for each action fluent.'''
+    def _build_policy_variables(self, initializers=None):
+        """Builds the policy variables for each action fluent."""
         action_fluents = self._compiler.rddl.domain.action_fluent_ordering
         action_size = self._compiler.rddl.action_size
         if initializers is None:
@@ -72,12 +67,10 @@ class OpenLoopPolicy(Policy):
         self._policy_variables = tuple(self._policy_variables)
 
     def __getitem__(self, i):
-        return [var[i,...] for var in self._policy_variables]
+        return [var[i, ...] for var in self._policy_variables]
 
-    def __call__(self,
-            state: Sequence[tf.Tensor],
-            timestep: tf.Tensor) -> Sequence[tf.Tensor]:
-        '''Returns action fluents for the current `state` and `timestep`.
+    def __call__(self, state, timestep):
+        """Returns action fluents for the current `state` and `timestep`.
 
         Args:
             state (Sequence[tf.Tensor]): The current state fluents.
@@ -85,30 +78,30 @@ class OpenLoopPolicy(Policy):
 
         Returns:
             Sequence[tf.Tensor]: A tuple of action fluents.
-        '''
+        """
+        # pylint: disable=too-many-locals
         action_fluents = self._compiler.rddl.domain.action_fluent_ordering
         action_size = self._compiler.rddl.action_size
         bounds = self._compiler.action_bound_constraints(state)
         action = []
         with self.graph.as_default():
-            t = tf.cast(timestep[0][0], tf.int32) # TODO change timestep dtype in tfrddlsim
-            for fluent, size, var in zip(action_fluents, action_size, self._policy_variables):
+            timestep = tf.cast(timestep[0][0], tf.int32)
+            for fluent, size, var in zip(
+                action_fluents, action_size, self._policy_variables
+            ):
                 lower, upper = bounds[fluent]
                 lower_batch = lower.batch if lower is not None else False
                 upper_batch = upper.batch if upper is not None else False
                 bounds_batch = lower_batch or upper_batch
-                tensor = self._get_action_tensor(var[:,t,...], (lower, upper))
+                tensor = self._get_action_tensor(var[:, timestep, ...], (lower, upper))
                 if not self.parallel_plans and not bounds_batch:
                     multiples = [self.batch_size] + [1] * len(size)
                     tensor = tf.tile(tensor, multiples)
                 action.append(tensor)
         return tuple(action)
 
-    def _get_policy_variable(self,
-            fluent: str,
-            fluent_shape: Sequence[int],
-            initializer=None) -> tf.Tensor:
-        '''Returns the correspondig policy variable for `fluent` with `fluent_shape`.
+    def _get_policy_variable(self, fluent, fluent_shape, initializer=None):
+        """Returns the correspondig policy variable for `fluent` with `fluent_shape`.
 
         Args:
             fluent (str): The fluent name.
@@ -116,29 +109,32 @@ class OpenLoopPolicy(Policy):
 
         Returns:
             tf.Tensor: The policy variable for the action fluent.
-        '''
+        """
         if self.parallel_plans:
             shape = [self.batch_size, self.horizon] + list(fluent_shape)
         else:
             shape = [1, self.horizon] + list(fluent_shape)
 
-        name = fluent.replace('/', '-') # TODO change canonical fluent name in tfrddlsim
+        name = fluent.replace("/", "-")
         if initializer is not None:
             initializer = tf.constant_initializer(initializer, dtype=tf.float32)
-        return tf.get_variable(name, dtype=tf.float32, shape=shape, initializer=initializer)
+        return tf.get_variable(
+            name, dtype=tf.float32, shape=shape, initializer=initializer
+        )
 
-    def _get_action_tensor(self, policy_variable: tf.Tensor, bounds: Bounds) -> tf.Tensor:
-        '''Returns the action tensor for `policy_variable` with domain
+    @staticmethod
+    def _get_action_tensor(policy_variable, bounds):
+        """Returns the action tensor for `policy_variable` with domain
         constrainted by the action fluent precondition `bounds`
 
         Args:
             policy_variable (tf.Tensor): The policy variable.
-            bounds (Tuple[Optional[tfrddlsim.fluent.TensorFluent],
-            Optional[tfrddlsim.fluent.TensorFluent]]): The (lower, upper) bounds.
+            bounds (Tuple[Optional[rddl2t.core.fluent.TensorFluent],
+            Optional[rddl2tf.core.fluent.TensorFluent]]): The (lower, upper) bounds.
 
         Returns:
             tf.Tensor: The action fluent tensor.
-        '''
+        """
         lower, upper = bounds
         lower = lower.cast(tf.float32).tensor if lower is not None else None
         upper = upper.cast(tf.float32).tensor if upper is not None else None
