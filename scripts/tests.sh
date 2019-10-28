e09c8b01629267a558c4d1c939eeafc989432020
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-10-28 16:01:56 -0300

    chore(experiments): update scripts/tests.sh script
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,5 +1,10 @@
-python3 experiments.py Reservoir-10 inst_reservoir_res10.baseline.txt
-python3 experiments.py Reservoir-20 inst_reservoir_res20.baseline.txt
-python3 experiments.py Reservoir-30 inst_reservoir_res30.baseline.txt
-python3 experiments.py HVAC-3 inst_hvac_vav_fix_3rooms.baseline.txt
-python3 experiments.py HVAC-6 inst_hvac_vav_fix_6rooms.baseline.txt
+#!/usr/bin/env bash
+
+tfplan -v straightline Navigation-v3 -h 15 -e 1000 -lr 0.01 --optimizer GradientDescent --logdir experiments/nav-v3/straightline/sgd/lr=1e-2/epochs=1000
+tfplan -v hindsight Navigation-v3 -h 15 -e 1000 -lr 0.01 --optimizer GradientDescent --logdir experiments/nav-v3/hindsight/sgd/lr=1e-2/epochs=1000
+
+tfplan -v straightline Navigation-v3 -h 15 -e 1000 -lr 0.01 --optimizer Adam --logdir experiments/nav-v3/straightline/adam/lr=1e-2/epochs=1000
+tfplan -v hindsight Navigation-v3 -h 15 -e 1000 -lr 0.01 --optimizer Adam --logdir experiments/nav-v3/hindsight/adam/lr=1e-2/epochs=1000
+
+tfplan -v straightline Navigation-v3 -h 15 -e 1000 -lr 0.01 --optimizer RMSProp --logdir experiments/nav-v3/straightline/rmsprop/lr=1e-2/epochs=1000
+tfplan -v hindsight Navigation-v3 -h 15 -e 1000 -lr 0.01 --optimizer RMSProp --logdir experiments/nav-v3/hindsight/rmsprop/lr=1e-2/epochs=1000
