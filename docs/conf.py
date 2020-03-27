eb4f2d7a17a7c921a1c0f543c90f674720a41390
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-03-27 17:23:45 -0300

    chore(sphinx): import tfplan in docs/conf.py
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import tfplan
+
 
 import sphinx_rtd_theme
 
@@ -34,11 +36,12 @@ release = '0.6.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx_rtd_theme',
 ]
 
-# Napoleon extension configuration
+# -- Extension configuration -------------------------------------------------
 napoleon_include_private_with_doc = True
 napoleon_include_special_with_doc = True
 
@@ -58,6 +61,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+
+import sphinx_rtd_theme
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
