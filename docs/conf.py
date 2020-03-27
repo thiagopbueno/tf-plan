2c574509dad3411b7d313377611f637a14b756a0
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-03-27 17:24:23 -0300

    chore(sphinx): import tfplan in docs/conf.py
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -14,10 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+
 import tfplan
-
-
-import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
