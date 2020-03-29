ac9314c61216fa970389be146e000b1fedccd1f0
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-03-28 22:03:21 -0300

    chore(docs): set autodoc_default_options in docs/conf.py
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@
 
 docs:
 	sphinx-apidoc -f -o docs tfplan
-	rm -R docs/_build/html && sphinx-build docs docs/_build/html
+	[ -e "docs/_build/html" ] && rm -R docs/_build/html
+	sphinx-build docs docs/_build/html
 
 test:
 	pytest tests/*.py -sv --disable-warnings 2>/dev/null
--- docs/conf.py
+++ docs/conf.py
@@ -20,12 +20,12 @@ import tfplan
 
 # -- Project information -----------------------------------------------------
 
-project = 'tf-plan'
-copyright = '2020, Thiago P. Bueno'
-author = 'Thiago P. Bueno'
+project = "tf-plan"
+copyright = "2020, Thiago P. Bueno"
+author = "Thiago P. Bueno"
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = "0.6.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -34,32 +34,27 @@ release = '0.6.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
+    "sphinx.ext.autodoc",
+    "sphinxcontrib.napoleon",
+    "sphinx_rtd_theme",
 ]
 
-# -- Extension configuration -------------------------------------------------
-napoleon_include_private_with_doc = True
-napoleon_include_special_with_doc = True
-
-
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -67,15 +62,23 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 import sphinx_rtd_theme
+
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Sort members by type
-autodoc_member_order = 'groupwise'
+# autodoc_member_order = 'groupwise'
+
+autodoc_default_options = {
+    "member-order": "bysource",
+    "special-members": True,
+    "private-members": True,
+    "special-members": True,
+}
