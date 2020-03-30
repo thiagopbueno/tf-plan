562f03e185834a14c317d594484d565f796bdb25
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-03-30 16:57:59 -0300

    fix: add sphinx napoleon to docs/requirements.txt
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 import tfplan
@@ -36,6 +38,7 @@ release = "0.6.0"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinxcontrib.napoleon",
+    "m2r",
     "sphinx_rtd_theme",
 ]
 
