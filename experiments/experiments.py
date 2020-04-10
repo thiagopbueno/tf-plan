91aa168bab1d8aac0128de8528d7da793f9dba68
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-04-10 18:53:44 -0300

    feat: load launcher and plotter configs from JSON files
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -11,24 +11,6 @@ import tuneconfig
 PLANNERS = ["straightline", "hindsight"]
 
 
-BASE_CONFIG = {
-    "batch_size": tuneconfig.grid_search([64]),
-    "horizon": 5,
-    "learning_rate": tuneconfig.grid_search([0.005]),
-    "epochs": tuneconfig.grid_search([100]),
-    "optimizer": tuneconfig.grid_search(["Adam", "RMSProp", "GradientDescent"])
-}
-
-
-PLOT_CONFIG = {
-    "targets": ["loss:0", "loss:4"],
-    "anchors": ["batch=64", "lr=0.005"],
-    "x_axis": None,
-    "y_axis": "optimizer",
-    "kwargs": {"target_x_axis_label": "Epochs", "target_y_axis_label": "Loss"}
-}
-
-
 def format_fn(param):
     fmt = {
         "batch_size": "batch",
@@ -54,6 +36,7 @@ def cli():
 
 @cli.command()
 @click.argument("rddl")
+@click.argument("config", type=click.Path())
 @click.option(
     "--num-samples",
     "-n",
@@ -85,24 +68,35 @@ def run(**kwargs):
     num_workers = kwargs["num_workers"]
     verbose = kwargs["verbose"]
 
-    BASE_CONFIG["rddl"] = rddl
-    BASE_CONFIG["num_samples"] = num_samples
-    BASE_CONFIG["num_workers"] = num_workers
-    BASE_CONFIG["verbose"] = False
+    config_factory = tuneconfig.ConfigFactory.from_json(
+        kwargs["config"], format_fn=format_fn
+    )
+
+    config_factory.update(
+        {
+            "rddl": rddl,
+            "num_samples": num_samples,
+            "num_workers": num_workers,
+            "verbose": False,
+        }
+    )
 
     for planner in PLANNERS:
         print(f"::: planner={planner} :::")
+
         logdir = os.path.join(base_dir, rddl, planner)
-        BASE_CONFIG["planner"] = planner
-        BASE_CONFIG["logdir"] = logdir
+
+        config_factory.update(
+            {"planner": planner, "logdir": logdir,}
+        )
 
         analysis = tuneconfig.run_experiment(
             tf_plan_runner,
-            tuneconfig.ConfigFactory(BASE_CONFIG, format_fn=format_fn),
+            config_factory,
             logdir,
             num_samples=num_samples,
             num_workers=num_workers,
-            verbose=verbose
+            verbose=verbose,
         )
         print()
         analysis.info()
@@ -110,17 +104,11 @@ def run(**kwargs):
 
 
 @cli.command()
+@click.argument("config", type=click.Path())
 @click.argument("paths", nargs=-1, required=True)
-@click.option(
-    "-s", "--show-fig",
-    is_flag=True,
-    help="Show figure."
-)
-@click.option(
-    "--filename",
-    help="Output filepath."
-)
-def plot(paths, show_fig, filename):
+@click.option("-s", "--show-fig", is_flag=True, help="Show figure.")
+@click.option("-o", "--output", type=click.Path(), help="Output filepath.")
+def plot(config, paths, show_fig, output):
     """Plot results."""
     prefix = os.path.commonprefix(paths)
 
@@ -134,20 +122,11 @@ def plot(paths, show_fig, filename):
         analysis_lst.append(analysis)
 
     plotter = tuneconfig.ExperimentPlotter(analysis_lst)
-    kwargs = {
-        **PLOT_CONFIG,
-        "show_fig": show_fig,
-        "filename": filename,
-    }
-    plotter.plot(**kwargs)
+    plotter.plot_chart_from_spec(config, show_fig=show_fig, filename=output)
 
 
 def tf_plan_runner(config):
     # pylint: disable=import-outside-toplevel
-
-    # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-    os.environ["OMP_NUM_THREADS"] = str(psutil.cpu_count(logical=False))
 
     import rddlgym
     import tfplan
@@ -155,6 +134,10 @@ def tf_plan_runner(config):
     import tensorflow as tf
 
     tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+    os.environ["OMP_NUM_THREADS"] = str(psutil.cpu_count(logical=False))
 
     planner = config["planner"]
     rddl = config["rddl"]
--- tfplan/version.py
+++ tfplan/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.7.0"
-__release__ = "v0.7.0-alpha"
+__version__ = "0.7.1"
+__release__ = "v0.7.1-alpha"
